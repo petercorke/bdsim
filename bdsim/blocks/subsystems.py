5306ef25efcf85b4b52ce89f15d20e0a378341e9
Peter Corke <peter.i.corke@gmail.com>
2020-06-01 21:59:57 +1000

    Added a mux block

--- bdsim/blocks/subsystems.py
+++ bdsim/blocks/subsystems.py
@@ -14,3 +14,19 @@ from bdsim.components import Sink, Source, Function
 At compile time we remove/disable certain wires.
 Block should have the subsystem enable status
 """
+
+from bdsim.components import *
+
+
+@block
+class _Mux(Function):
+    def __init__(self, nin=1, **kwargs):
+
+        super().__init__(**kwargs)
+        self.nin = nin
+        self.nout = 1
+        self.type = 'mux'
+    
+    def output(self, t=None):
+        # TODO, handle inputs that are vectors themselves
+        return [ np.r_[self.inputs] ]
