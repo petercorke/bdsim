e85baa26d319b560ca5aa9ceb2776471f280e1e3
Peter Corke <peter.i.corke@gmail.com>
2020-06-01 21:59:12 +1000

    added limiters to inputs as per the MATLAB version

--- bdsim/blocks/robot.py
+++ bdsim/blocks/robot.py
@@ -9,7 +9,7 @@ from bdsim.components import *
 
 @block
 class _Bicycle(Transfer):
-    def __init__(self, x0=None, L=1, **kwargs):
+    def __init__(self, x0=None, L=1, vlim=1, slim=1, **kwargs):
         """
         
         :param x0: DESCRIPTION, defaults to None
@@ -22,48 +22,65 @@ class _Bicycle(Transfer):
         :rtype: TYPE
 
         """
+        self._names_in=['v', '$\gamma$']
+        self._names_out=['x', 'y', '$\theta$']
+        self._names_state = ['x', 'y', '$\theta$']
         super().__init__(**kwargs)
         self.nin = 2
         self.nout = 3
         self.nstates = 3
+        self.vlim = vlim
+        self.slim = slim
+        self.type = 'bicycle'
+
+        
         self.L = L
         if x0 is None:
-            self.x0 = np.zeros((slef.nstates,))
+            self._x0 = np.zeros((self.nstates,))
         else:
             assert len(x0) == self.nstates, "x0 is {:d} long, should be {:d}".format(len(x0), self.nstates)
-            self.x0 = x0
+            self._x0 = x0
         
     def output(self, t):
-        return list(self.x)
+        return list(self._x)
     
     def deriv(self):
-        theta = self.x[2]
-        v = self.inputs[0]; gamma = self.inputs[1]
+        theta = self._x[2]
+        
+        # get inputs and clip them
+        v = self.inputs[0]
+        v = min(self.vlim, max(v, -self.vlim))
+        gamma = self.inputs[1]
+        gamma = min(self.slim, max(gamma, -self.slim))
+        
         xd = np.r_[v*math.cos(theta), v*math.sin(theta), v*math.tan(gamma)/self.L ]
         return xd
     
+@block
 class _Unicycle(Transfer):
-    def __init__(self, x0=None, L=1, **kwargs):
+    def __init__(self, x0=None, **kwargs):
         super().__init__(**kwargs)
         self.nin = 2
         self.nout = 3
         self.nstates = 3
+        self.type = 'unicycle'
+        
         if x0 is None:
-            self.x0 = np.zeros((slef.nstates,))
+            self._x0 = np.zeros((slef.nstates,))
         else:
             assert len(x0) == self.nstates, "x0 is {:d} long, should be {:d}".format(len(x0), self.nstates)
-            self.x0 = x0
+            self._x0 = x0
         
     def output(self, t):
-        return list(self.x)
+        return list(self._x)
     
     def deriv(self):
-        theta = self.x[2]
+        theta = self._x[2]
         v = self.inputs[0]; omega = self.inputs[1]
         xd = np.r_[v*math.cos(theta), v*math.sin(theta), omega]
         return xd
     
-    # unicycle
+    # diffsteer
     # quadrotor
     # quadrotor plot
     # seriallink
