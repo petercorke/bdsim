d4d578c094de5ec3e7d14f6f8acf4497200a5de6
Peter Corke <peter.i.corke@gmail.com>
2020-07-02 20:44:22 +1000

    Change figure update code so that figures don't keep taking focus, reorder the methods

--- bdsim/blocks/sinks.py
+++ bdsim/blocks/sinks.py
@@ -52,6 +52,179 @@ class Print(SinkBlock):
     def step(self):
         print('PRINT {:s} = '.format(self.name), self.inputs)
 
+# ------------------------------------------------------------------------ #
+
+
+@block
+class Scope(GraphicsBlock):
+    """
+    :blockname:`SCOPE`
+
+    Create a block that plots input ports against time.
+    
+    :param nin: number of inputs, defaults to length of style vector if given,
+                otherwise 1
+    :type nin: int, optional
+    :param styles: styles for each line to be plotted
+    :type styles: optional str or dict, list of strings or dicts; one per line
+    :param scale: y-axis scale, defaults to 'auto'
+    :type scale: 2-element sequence
+    :param labels: vertical axis labels
+    :type labels: sequence of strings
+    :param grid: draw a grid, default is on. Can be boolean or a tuple of 
+                 options for grid()
+    :type grid: bool or sequence
+    :param ``*inputs``: Optional incoming connections
+    :type ``*inputs``: Block or Plug
+    :param ``**kwargs``: common Block options
+    :return: A SCOPE block
+    :rtype: Scope instance
+
+    Create a block that plots input ports against time.  
+
+    Each line can have its own color or style which is specified by:
+    
+        - a dict of options for `plot` or 
+        - a  MATLAB-style linestyle like 'k--'
+    
+    If multiple lines are plotted then a heterogeneous list of styles, dicts or strings,
+    one per line must be given.
+    
+    The vertical scale factor defaults to auto-scaling but can be fixed by
+    providing a 2-tuple [ymin, ymax]. All lines are plotted against the
+    same vertical scale.
+    
+    Examples::
+        
+        SCOPE()
+        SCOPE(nin=2)
+        SCOPE(nin=2, scale=[-1,2])
+        SCOPE(style=['k', 'r--'])
+        SCOPE(style='k--')
+        SCOPE(style={'color:', 'red, 'linestyle': '--''})
+        
+    .. figure:: ../../figs/Figure_1.png
+       :width: 500px
+       :alt: example of generated graphic
+
+       Example of scope display.
+    """
+    
+    def __init__(self, nin=None, styles=None, scale='auto', labels=None, grid=True, *inputs, **kwargs):
+
+
+        self.type = 'scope'
+        if styles is not None:
+            self.styles = list(styles)
+            if nin is not None:
+                assert nin == len(styles), 'need one style per input'
+            else:
+                nin = len(styles)
+
+        if labels is not None:
+            self.labels = list(labels)
+            if nin is not None:
+                assert nin == len(labels), 'need one label per input'
+            else:
+                nin = len(labels)
+        else:
+            self.labels = labels
+
+        if nin is None:
+            nin = 1
+        
+        super().__init__(nin=nin, inputs=inputs, **kwargs)
+
+        
+        if styles is None:
+            self.styles = [ None ] * nin
+        
+        self.grid = grid
+                 
+        # init the arrays that hold the data
+        self.tdata = np.array([])
+        self.ydata = [np.array([]),] * nin
+        
+        self.line = [None]*nin
+        self.scale = scale
+        
+        self.labels = labels
+        # TODO, wire width
+        # inherit names from wires, block needs to be able to introspect
+        
+    def start(self, **kwargs):
+        # create the plot
+        if self.bd.graphics:
+            super().reset()   # TODO should this be here?
+            self.fig = self.bd.create_figure()
+            self.ax = self.fig.gca()
+            # figure out the labels
+            if self.labels is None:
+                self.labels = [self.sourcename(i) for i in range(0, self.nin)]
+                self.labels.insert(0, 'Time')
+            elif len(self.labels) == 1:
+                self.labels += [self.sourcename(i) for i in range(0, self.nin)]
+            else:
+                raise ValueError('incorrect number of labels specified for Scope')
+            for i in range(0, self.nin):
+                args = []
+                kwargs = {}
+                style = self.styles[i]
+                if isinstance(style, dict):
+                    kwargs = style
+                elif isinstance(style, str):
+                    args = [style]
+                self.line[i], = self.ax.plot(self.tdata, self.ydata[i], *args, label=self.styles[i], **kwargs)
+            self.ax.set_ylabel(','.join(self.labels[1:]))
+            if self.grid is True:
+                self.ax.grid(self.grid)
+            elif isinstance(self.grid, (list, tuple)):
+                self.ax.grid(True, *self.grid)
+                
+            self.ax.set_xlim(0, self.bd.T)
+            # self.ax.set_ylim(-2, 2)
+            self.ax.set_xlabel(self.labels[0])
+
+            self.ax.set_title(self.name)
+            if self.scale != 'auto':
+                self.ax.set_ylim(*self.scale)
+            if self.labels is not None:
+                self.ax.legend(self.labels[1:])
+
+            plt.draw()
+            plt.show(block=False)
+
+            super().start()
+        
+    def step(self):
+        # inputs are set
+        if self.bd.graphics:
+            self.tdata = np.append(self.tdata, self.bd.t)
+            for i,input in enumerate(self.inputs):
+                self.ydata[i] = np.append(self.ydata[i], input)
+            if self.bd.graphics:
+                plt.figure(self.fig.number)
+                for i in range(0, self.nin):
+                    self.line[i].set_data(self.tdata, self.ydata[i])
+            
+                # plt.draw()
+                # plt.show(block=False)
+                # if self.bd.animation:
+                #     self.fig.canvas.start_event_loop(0.001)
+                self.fig.canvas.flush_events()
+            
+                if self.scale == 'auto':
+                    self.ax.relim()
+                    self.ax.autoscale_view(scalex=False, scaley=True)
+                super().step()
+        
+    def done(self, block=False, **kwargs):
+        if self.bd.graphics:
+            plt.show(block=block)
+            super().done()
+
+# ------------------------------------------------------------------------ #
+
 @block
 class ScopeXY(GraphicsBlock):
     """
@@ -130,6 +303,10 @@ class ScopeXY(GraphicsBlock):
                 self.ax.set_ylim(*self.scale[2:4])
             if self.init is not None:
                 self.init(self.ax)
+
+            plt.draw()
+            plt.show(block=False)
+
             super().start()
 
         
@@ -141,10 +318,12 @@ class ScopeXY(GraphicsBlock):
             plt.figure(self.fig.number)
             self.line.set_data(self.xdata, self.ydata)
         
-            plt.draw()
-            plt.show(block=False)
-            if self.bd.animation:
-                self.fig.canvas.start_event_loop(0.001)
+            # plt.draw()
+            # plt.show(block=False)
+            # if self.bd.animation:
+            #     self.fig.canvas.start_event_loop(0.001)
+            self.fig.canvas.flush_events()
+
         
             if self.scale == 'auto':
                 self.ax.relim()
@@ -231,7 +410,7 @@ class Vehicle(GraphicsBlock):
             self.ax = self.fig.gca()
             if self.square:
                 self.ax.set_aspect('equal')
-            
+
             args = []
             kwargs = {}
             if self.path:
@@ -253,6 +432,10 @@ class Vehicle(GraphicsBlock):
                 self.ax.set_ylim(*self.scale[2:4])
             if self.init is not None:
                 self.init(self.ax)
+                
+            plt.draw()
+            plt.show(block=False)
+
             super().start()
         
     def step(self):
@@ -260,17 +443,18 @@ class Vehicle(GraphicsBlock):
         if self.bd.graphics:
             self.xdata.append(self.inputs[0])
             self.ydata.append(self.inputs[1])
-            plt.figure(self.fig.number)
+            #plt.figure(self.fig.number)
             if self.path:
                 self.line.set_data(self.xdata, self.ydata)
             T = sm.transl2(self.inputs[0], self.inputs[1]) @ sm.trot2(self.inputs[2])
             new = sm.h2e(T @ self.vertices_hom)
             self.vehicle.set_xy(new.T)
         
-            plt.draw()
-            plt.show(block=False)
-            if self.bd.animation:
-                self.fig.canvas.start_event_loop(0.001)
+            # plt.draw()
+            # plt.show(block=False)
+            # if self.bd.animation:
+            #     self.fig.canvas.start_event_loop(0.001)
+            self.fig.canvas.flush_events()
         
             if self.scale == 'auto':
                 self.ax.relim()
@@ -483,171 +667,6 @@ class MultiRotorPlot(GraphicsBlock):
         plot3(self.groundmark, z[0], -z[1], 0)
 
 
-# ------------------------------------------------------------------------ #
-
-
-@block
-class Scope(GraphicsBlock):
-    """
-    :blockname:`SCOPE`
-
-    Create a block that plots input ports against time.
-    
-    :param nin: number of inputs, defaults to length of style vector if given,
-                otherwise 1
-    :type nin: int, optional
-    :param styles: styles for each line to be plotted
-    :type styles: optional str or dict, list of strings or dicts; one per line
-    :param scale: y-axis scale, defaults to 'auto'
-    :type scale: 2-element sequence
-    :param labels: vertical axis labels
-    :type labels: sequence of strings
-    :param grid: draw a grid, default is on. Can be boolean or a tuple of 
-                 options for grid()
-    :type grid: bool or sequence
-    :param ``*inputs``: Optional incoming connections
-    :type ``*inputs``: Block or Plug
-    :param ``**kwargs``: common Block options
-    :return: A SCOPE block
-    :rtype: Scope instance
-
-    Create a block that plots input ports against time.  
-
-    Each line can have its own color or style which is specified by:
-    
-        - a dict of options for `plot` or 
-        - a  MATLAB-style linestyle like 'k--'
-    
-    If multiple lines are plotted then a heterogeneous list of styles, dicts or strings,
-    one per line must be given.
-    
-    The vertical scale factor defaults to auto-scaling but can be fixed by
-    providing a 2-tuple [ymin, ymax]. All lines are plotted against the
-    same vertical scale.
-    
-    Examples::
-        
-        SCOPE()
-        SCOPE(nin=2)
-        SCOPE(nin=2, scale=[-1,2])
-        SCOPE(style=['k', 'r--'])
-        SCOPE(style='k--')
-        SCOPE(style={'color:', 'red, 'linestyle': '--''})
-        
-    .. figure:: ../../figs/Figure_1.png
-       :width: 500px
-       :alt: example of generated graphic
-
-       Example of scope display.
-    """
-    
-    def __init__(self, nin=None, styles=None, scale='auto', labels=None, grid=True, *inputs, **kwargs):
-
-
-        self.type = 'scope'
-        if styles is not None:
-            self.styles = list(styles)
-            if nin is not None:
-                assert nin == len(styles), 'need one style per input'
-            else:
-                nin = len(styles)
-
-        if labels is not None:
-            self.labels = list(labels)
-            if nin is not None:
-                assert nin == len(labels), 'need one label per input'
-            else:
-                nin = len(labels)
-        else:
-            self.labels = labels
-
-        if nin is None:
-            nin = 1
-        
-        super().__init__(nin=nin, inputs=inputs, **kwargs)
-
-        
-        if styles is None:
-            self.styles = [ None ] * nin
-        
-        self.grid = grid
-                 
-        # init the arrays that hold the data
-        self.tdata = np.array([])
-        self.ydata = [np.array([]),] * nin
-        
-        self.line = [None]*nin
-        self.scale = scale
-        
-        self.labels = labels
-        # TODO, wire width
-        # inherit names from wires, block needs to be able to introspect
-        
-    def start(self, **kwargs):
-        # create the plot
-        if self.bd.graphics:
-            super().reset()   # TODO should this be here?
-            self.fig = self.bd.create_figure()
-            self.ax = self.fig.gca()
-            # figure out the labels
-            if self.labels is None:
-                self.labels = [self.sourcename(i) for i in range(0, self.nin)]
-                self.labels.insert(0, 'Time')
-            elif len(self.labels) == 1:
-                self.labels += [self.sourcename(i) for i in range(0, self.nin)]
-            else:
-                raise ValueError('incorrect number of labels specified for Scope')
-            for i in range(0, self.nin):
-                args = []
-                kwargs = {}
-                style = self.styles[i]
-                if isinstance(style, dict):
-                    kwargs = style
-                elif isinstance(style, str):
-                    args = [style]
-                self.line[i], = self.ax.plot(self.tdata, self.ydata[i], *args, label=self.styles[i], **kwargs)
-            self.ax.set_ylabel(','.join(self.labels[1:]))
-            if self.grid is True:
-                self.ax.grid(self.grid)
-            elif isinstance(self.grid, (list, tuple)):
-                self.ax.grid(True, *self.grid)
-                
-            self.ax.set_xlim(0, self.bd.T)
-            # self.ax.set_ylim(-2, 2)
-            self.ax.set_xlabel(self.labels[0])
-
-            self.ax.set_title(self.name)
-            if self.scale != 'auto':
-                self.ax.set_ylim(*self.scale)
-            if self.labels is not None:
-                self.ax.legend(self.labels[1:])
-            super().start()
-        
-    def step(self):
-        # inputs are set
-        if self.bd.graphics:
-            self.tdata = np.append(self.tdata, self.bd.t)
-            for i,input in enumerate(self.inputs):
-                self.ydata[i] = np.append(self.ydata[i], input)
-            if self.bd.graphics:
-                plt.figure(self.fig.number)
-                for i in range(0, self.nin):
-                    self.line[i].set_data(self.tdata, self.ydata[i])
-            
-                plt.draw()
-                plt.show(block=False)
-                if self.bd.animation:
-                    self.fig.canvas.start_event_loop(0.001)
-            
-                if self.scale == 'auto':
-                    self.ax.relim()
-                    self.ax.autoscale_view(scalex=False, scaley=True)
-                super().step()
-        
-    def done(self, block=False, **kwargs):
-        if self.bd.graphics:
-            plt.show(block=block)
-            super().done()
 
 # ------------------------------------------------------------------------ #
             
