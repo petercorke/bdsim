8eb20caf06b2944cf661631a0a091a9a06c9afde
Daniel Petkov <daniel.petkov@connect.qut.edu.au>
2021-06-08 10:08:37 +1000

    Doc comments finalised for all classes and methods

--- bdsim/bdedit/interface_serialize.py
+++
@@ -1,9 +0,0 @@
-class Serializable:
-    def __init__(self):
-        self.id = id(self)
-
-    def serialize(self):
-        raise NotImplemented()
-
-    def deserialize(self, data, hashmap={}):
-        raise NotImplemented()
