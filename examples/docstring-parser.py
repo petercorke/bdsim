62c96b9b2302a84f17ddfd216bb1aad9c8773238
Peter Corke <peter.i.corke@gmail.com>
2021-07-14 06:30:15 +1000

    add block URL and classname to the dict

--- examples/docstring-parser.py
+++ examples/docstring-parser.py
@@ -1,6 +1,8 @@
 import bdsim
 import re
 from collections import OrderedDict
+import inspect
+import importlib.util
 
 re_isfield = re.compile(r'\s*:[a-zA-Z0-9_ ]+:')
 re_field = re.compile(r'^\s*:(?P<field>[a-zA-Z]+)(?: +(?P<var>[a-zA-Z0-9_]+))?:(?P<body>.+)$', re.MULTILINE)
@@ -8,103 +10,121 @@ re_field = re.compile(r'^\s*:(?P<field>[a-zA-Z]+)(?: +(?P<var>[a-zA-Z0-9_]+))?:(
 def indent(s):
     return len(s) - len(s.lstrip())
 
-sim = bdsim.BDSim(verbose=True)
 
 blocks = {}
 
 fieldnames = ('param', 'type', 'input', 'output', 'varinputs')
 excludevars = ('kwargs', 'inputs')
 
-for name, block, path in sim.blocklibrary:
-
-    # create a dict of block info
-    block_info = {}
-    block_info['path'] = path
-
-    # get the docstring
-    ds = block.__init__.__doc__
-
-    # parse out all lines of the form:
-    #
-    #  :field var: body
-    # or
-    #  :field var: body with a very long description that
-    #       carries over to another line or two
-    fieldlines = []
-    for para in ds.split('\n\n'):
-        # print(para)
-        # print('--')
-
-        indent_prev = None
-        infield = False
-
-        for line in para.split('\n'):
-            if len(line) == 0:
-                continue
-            if indent_prev is None:
-                indent_prev = indent(line)
-            if re_isfield.match(line) is not None:
-                fieldlines.append(line.lstrip())
-                infield = True
-            if indent(line) > indent_prev and infield:
-                fieldlines[-1] += ' ' + line.lstrip()
-            if indent(line) == indent_prev:
-                infield = False
-
-    # fieldlines is a list of lines of the form
-    #
-    #   :field var: body
-    #
-    # where extension lines have been concatenated
-
-    # create a dict of dicts
-    #
-    #   dict[field][var] -> body
-    dict = OrderedDict()
-
-    for line in fieldlines:
-        m = re_field.match(line)
-        if m is not None:
-            field, var, body = m.groups()
-            if var in excludevars or field not in fieldnames:
-                continue
-            if field not in dict:
-                dict[field] = {var: body}
-            else:
-                dict[field][var] = body
-            dict[m.group('field')]
-
-    # now connect pairs of lines of the form
-    #
-    # :param X: param description
-    # :type X: type description
-    #
-    # params[X] = (type description, param description)
-    params = {}
-    if 'param' in dict:
-        for var, descrip in dict['param'].items():
-            typ = dict['type'].get(var, None)
-            params[var] = (typ, descrip)
-
-    # now add all the other stuff we know about the block
-    block_info['params'] = params
-    block_info['inputs'] = dict.get('input')
-    block_info['outputs'] = dict.get('output')
-    block_info['varinputs'] = 'varinputs' in dict
+# sim = bdsim.BDSim(verbose=True)
+for package in ('bdsim',): #, 'roboticstoolbox'):
+    spec = importlib.util.find_spec('.blocks', package=package)
+    m = spec.loader.load_module()
+    path = m.__path__
     try:
-        instance = block()
-        block_info['nin'] = instance.nin
-        block_info['nout'] = instance.nout
-        block_info['blockclass'] = instance.blockclass
-    except:
+       url = m.__dict__['url']
+    except KeyError:
+        url = None
 
-        print('couldnt instantiate ', name)
-        block_info['nin'] = 1
-        block_info['nout'] = 1
-        blockclass = block.__base__.__name__.lower().replace('block', '')
-        block_info['blockclass'] = blockclass
+    for name, block in m.__dict__.items():
+        # check if it's a valid block class
+        if not inspect.isclass(block):
+            continue
+        if inspect.getmro(block)[-2].__name__ != 'Block':
+            continue
+        if name.endswith('Block'):
+            continue
+
+        # create a dict of block info
+        block_info = {}
+        block_info['path'] = path  # path to folder holding block definition
+        block_info['classname'] = name
+        block_info['url'] = url + "#" + block.__module__ + "." + name
+
+        # get the docstring
+        ds = block.__init__.__doc__ #inspect.getdoc(block)
+
+        # parse out all lines of the form:
+        #
+        #  :field var: body
+        # or
+        #  :field var: body with a very long description that
+        #       carries over to another line or two
+        fieldlines = []
+        for para in ds.split('\n\n'):
+            # print(para)
+            # print('--')
+
+            indent_prev = None
+            infield = False
+
+            for line in para.split('\n'):
+                if len(line) == 0:
+                    continue
+                if indent_prev is None:
+                    indent_prev = indent(line)
+                if re_isfield.match(line) is not None:
+                    fieldlines.append(line.lstrip())
+                    infield = True
+                if indent(line) > indent_prev and infield:
+                    fieldlines[-1] += ' ' + line.lstrip()
+                if indent(line) == indent_prev:
+                    infield = False
+
+        # fieldlines is a list of lines of the form
+        #
+        #   :field var: body
+        #
+        # where extension lines have been concatenated
+
+        # create a dict of dicts
+        #
+        #   dict[field][var] -> body
+        dict = OrderedDict()
+
+        for line in fieldlines:
+            m = re_field.match(line)
+            if m is not None:
+                field, var, body = m.groups()
+                if var in excludevars or field not in fieldnames:
+                    continue
+                if field not in dict:
+                    dict[field] = {var: body}
+                else:
+                    dict[field][var] = body
+                dict[m.group('field')]
+
+        # now connect pairs of lines of the form
+        #
+        # :param X: param description
+        # :type X: type description
+        #
+        # params[X] = (type description, param description)
+        params = {}
+        if 'param' in dict:
+            for var, descrip in dict['param'].items():
+                typ = dict['type'].get(var, None)
+                params[var] = (typ, descrip)
+
+        # now add all the other stuff we know about the block
+        block_info['params'] = params
+        block_info['inputs'] = dict.get('input')
+        block_info['outputs'] = dict.get('output')
+        block_info['varinputs'] = 'varinputs' in dict
+        try:
+            instance = block()
+            block_info['nin'] = instance.nin
+            block_info['nout'] = instance.nout
+            block_info['blockclass'] = instance.blockclass
+        except:
+
+            print('couldnt instantiate ', name)
+            block_info['nin'] = 1
+            block_info['nout'] = 1
+            blockclass = block.__base__.__name__.lower().replace('block', '')
+            block_info['blockclass'] = blockclass
 
 
-    blocks[name] = block_info
+        blocks[name] = block_info
 
 print(blocks)
