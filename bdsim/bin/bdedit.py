d98213d456358810bd6de627e61d2a29dd6ef1be
Peter Corke <peter.i.corke@gmail.com>
2021-07-27 21:21:11 +1000

    add command line argument handler

--- bdsim/bin/bdedit.py
+++ bdsim/bin/bdedit.py
@@ -2,6 +2,7 @@
 
 # Library imports
 import sys
+import argparse
 
 # PyQt5 imports
 from PyQt5.QtWidgets import *
@@ -12,8 +13,31 @@ from bdsim.bdedit.interface import Interface
 
 # Executable code to launch the BdEdit application window
 if __name__ == '__main__':
+
+    # handle command line options, bdedit -h for details
+    parser = argparse.ArgumentParser(description='Interactive edit for bdsim models')
+    parser.add_argument('file', type=str, nargs='?',
+        help='Load this model into interactive session')
+    parser.add_argument('--print', '-p', 
+        action='store_const', const=True, default=False,
+        help='Save model to screenshot and exit')
+    parser.add_argument('--debug', '-d', 
+        action='store_const', const=True, default=False,
+        help='Enable debugging')
+    args, unparsed_args = parser.parse_known_args()
+    
+    # args holds all the command line info:
+    #  args['file'] file name if given, else None
+    #  args['debug'] True if -d option given
+    #  args['print'] True if -p option given, load the file, save screenshot,
+    #                then exit
+
+    # insert argv[0] into head of list of remaining args, and hand that to Qt
+    unparsed_args.insert(0, sys.argv[0])
+
     # A QApplication instance is made, which is the window that holds everything
-    app = QApplication(sys.argv)
+    app = QApplication(unparsed_args)
+    # app = QApplication(sys.argv)
 
     # The resolution of the user's screen is extracted (used for determining
     # the size of the application window)
