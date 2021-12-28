0a89ec52b7ff126b45ef2f7f319d1392facdc167
Peter Corke <peter.i.corke@gmail.com>
2021-12-28 13:15:57 +1000

    adapt to recent changes in bike model

--- examples/rvc4_2.py
+++ examples/rvc4_2.py
@@ -4,27 +4,25 @@
 
 import bdsim
 
-sim = bdsim.BDSim(verbose=True)
+sim = bdsim.BDSim(animation=True)
 bd = sim.blockdiagram()
     
 steer = bd.PIECEWISE( (0,0), (3,0.5), (4,0), (5,-0.5), (6,0), name='steering')
 speed = bd.CONSTANT(1, name='speed')
 bike = bd.BICYCLE(x0=[0, 0, 0], name='bicycle')
+q = bd.DEMUX(3)
 
 tscope= bd.SCOPE(name='theta')
-scope = bd.SCOPEXY(scale=[0, 10, 0, 1.2])
+vplot = bd.VEHICLEPLOT(scale=[0, 10, -5, 5], size=0.7, shape='box') #, movie='rvc4_2.mp4')
 
+bd.connect(bike, q, vplot)
 bd.connect(speed, bike[0])
 bd.connect(steer, bike[1])
-
-bd.connect(bike[0:2], scope)
-bd.connect(bike[2], tscope)
+bd.connect(q[2], tscope)
 
 bd.compile()
 bd.report()
 
 out = sim.run(bd, dt=0.05)
 
-sim.savefigs(bd, format='pdf')
-
-bd.done(block=True)
+#sim.done(bd, block=True)
