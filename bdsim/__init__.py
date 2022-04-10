1638d42fedb23e5264ccb1f5ac5db614bde8997c
Peter Corke <peter.i.corke@gmail.com>
2022-04-10 13:17:51 +1000

    fix up package paths

--- bdsim/__init__.py
+++ bdsim/__init__.py
@@ -1,5 +1,11 @@
-from bdsim.bdsim import *
-from bdsim.blockdiagram import *
-from bdsim.components import *
-from bdsim.graphics import GraphicsBlock
-from bdsim.bdrun import bdrun, bdload
+# from bdsim.bdsim import *
+# from bdsim.blockdiagram import *
+# from bdsim.components import *
+# from bdsim.graphics import GraphicsBlock
+# from bdsim.bdrun import bdrun, bdload
+
+from .bdsim import *
+from .blockdiagram import *
+from .components import *
+from .graphics import GraphicsBlock
+from .bdrun import bdrun, bdload
