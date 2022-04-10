392a14c1a3d2b2c72db4bdc50694cc808639830f
Peter Corke <peter.i.corke@gmail.com>
2022-04-10 14:33:48 +1000

    bring up to date movie off by default, issues with this for windows

--- examples/eg1.py
+++ examples/eg1.py
@@ -2,17 +2,19 @@
 
 import bdsim
 
-sim = bdsim.BDSim()  # create simulator
+sim = bdsim.BDSim(debug='g', animation=True)  # create simulator
 bd = sim.blockdiagram()  # create an empty block diagram
-
 sim.blocks()
+
+print(sim.options)
+sim.set_options(animation=True)
 
 # define the blocks
 demand = bd.STEP(T=1, name='demand')
 sum = bd.SUM('+-')
 gain = bd.GAIN(10)
 plant = bd.LTI_SISO(0.5, [2, 1], name='plant')
-scope = bd.SCOPE(styles=['k', 'r--'], movie='eg1.mp4')
+scope = bd.SCOPE(styles=['k', 'r--']) #, movie='eg1.mp4')
 
 # connect the blocks
 bd.connect(demand, sum[0], scope[1])
@@ -22,11 +24,10 @@ bd.connect(gain, plant)
 bd.connect(plant, scope[0])
 
 bd.compile()   # check the diagram
-bd.report()    # list all blocks and wires
+bd.report_summary()    # list all blocks and wires
 
-out = sim.run(bd, 5)  # simulate for 5s
+out = sim.run(bd)  # simulate for 5s
 # out = sim.run(bd, 5 watch=[plant,demand])  # simulate for 5s
 print(out)
 
 # sim.savefig(scope, 'scope0') # save scope figure as scope0.pdf
-sim.done(bd, block=True)  # keep figures open on screen
