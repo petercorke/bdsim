e384ffbf1fd82aef9df4e95ddb9b4f7c794a0c4a
GitHub Action <action@github.com>
2023-05-01 07:11:45 +0000

    Update documentation

---
+++ bdsim-blocks-1.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+plt.plot([0, 2, 2,   3,   3, 4,   4,   5,   5, 5.2], 
+         [0, 0, 0.5, 0.5, 0, 0, -0.5, -0.5, 0, 0], lw=2)
+plt.grid(True)
---
+++ bdsim-blocks-2.py
@@ -0,0 +1,3 @@
+import matplotlib.pyplot as plt
+plt.plot([0, 2, 5], [-1, -1, 5], lw=2)
+plt.grid(True)
