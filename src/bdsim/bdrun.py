d964aec0d996622bc0546c44832bc710237f7d78
Peter Corke <peter.i.corke@gmail.com>
2026-05-07 16:48:28 +1000

    remove bdrun shim

--- src/bdsim/bdrun.py
+++
@@ -1,7 +0,0 @@
-# Backwards-compatibility shim.  bdrun has moved to bdsim.bin.bdrun.
-# Importing from this module (e.g. ``from bdsim.bdrun import bdrun, bdload``)
-# continues to work.
-from bdsim.bin.bdrun import (  # noqa: F401
-    bdload,
-    bdrun,
-)
